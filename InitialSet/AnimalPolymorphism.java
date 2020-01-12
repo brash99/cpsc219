77fe0bfc85d3e5f1fb6d30d4ae662b5a05e5ff5a
Edward Brash <brash@jlab.org>
2020-01-11 23:27:15 -0500

    Updates

---
+++ InitialSet/AnimalPolymorphism.java
@@ -0,0 +1,36 @@
+abstract class Animal {
+  public int legs;
+  public abstract void animalSound();  
+}
+
+class Pig extends Animal {
+  public int legs = 4;
+  public void animalSound() {
+    System.out.println("The pig says: wee wee");
+  }
+}
+
+class Dog extends Animal {
+  public int legs = 4;
+  public void animalSound() {
+	  System.out.println("The dog says: woof woof");
+  }
+}
+
+class Human extends Animal {
+	public int legs = 2;
+	public void animalSound() {
+		System.out.println("The human says: wazzzupppp");
+	}
+}
+
+class AnimalPolymorphism {
+  public static void main(String[] args) {
+    Pig myPig = new Pig();  // Create a Pig object
+    Dog myDog = new Dog();  // Create a Dog object
+    Human myHuman = new Human();  // Create a generic animal object
+    myPig.animalSound();
+    myDog.animalSound();
+    myHuman.animalSound();
+  }
+}
