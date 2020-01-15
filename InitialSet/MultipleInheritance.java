35ed89ad62128cc1ccc2ac3caf5a76f06cc70b63
Edward Brash <brash@jlab.org>
2020-01-14 23:05:44 -0500

    Updates

--- InitialSet/MultipleInheritance.java
+++ InitialSet/MultipleInheritance.java
@@ -1,15 +1,23 @@
 // First Parent class 
 interface Parent1 { 
     default void fun() { 
-        System.out.println("Parent1"); 
+        System.out.println("Parent1 fun()"); 
     } 
+    
+    default void parent1function() {
+    	System.out.println("Parent 1 unique function");
+    }
 } 
   
 // Second Parent Class 
 interface Parent2 { 
     default void fun() { 
-        System.out.println("Parent2"); 
+        System.out.println("Parent2 fun()"); 
     } 
+    
+    default void parent2function() {
+    	System.out.println("Parent 2 unique function");
+    }
 } 
 
 //
@@ -26,6 +34,7 @@ class MultipleInheritance implements Parent1, Parent2 {
 	   // keyword to make the connection with the parent class
 	   //
 	   int ichoice = 1;
+	   
 	   if (ichoice == 1) {
 		   Parent1.super.fun();
 	   } else {
@@ -35,6 +44,8 @@ class MultipleInheritance implements Parent1, Parent2 {
 	
    public static void main(String args[]) { 
        MultipleInheritance t = new MultipleInheritance();
-       t.fun(); 
+       t.fun();
+       t.parent1function();
+       t.parent2function();
    } 
 } 
