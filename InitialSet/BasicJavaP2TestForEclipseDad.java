665ad32d9be0d07e9e8cf8e269937ec8c5fe5a76
Edward Brash <brash@jlab.org>
2020-01-25 15:05:50 -0500

    rename some things

--- InitialSet/BasicJavaP2TestForEclipseDad.java
+++ InitialSet/BasicJavaP2TestForEclipseDad.java
@@ -81,7 +81,7 @@ public class BasicJavaP2TestForEclipseDad {
 		assertFalse("Don't use the Character class in your code.",usesLibrary("Character"));
 		
 		boolean expected = true;
-		boolean actual = BasicJavaP2.isUpper('A');
+		boolean actual = BasicJavaP2Dad.isUpper('A');
 		
 		assertEquals("Checking if A is upper case letter", expected, actual);
 	}
@@ -91,7 +91,7 @@ public class BasicJavaP2TestForEclipseDad {
 		assertFalse("Don't use the Character class in your code.",usesLibrary("Character"));
 		
 		boolean expected = true;
-		boolean actual = BasicJavaP2.isUpper('Z');
+		boolean actual = BasicJavaP2Dad.isUpper('Z');
 		
 		assertEquals("Checking if Z is upper case letter", expected, actual);
 	}
@@ -101,7 +101,7 @@ public class BasicJavaP2TestForEclipseDad {
 		assertFalse("Don't use the Character class in your code.",usesLibrary("Character"));
 		
 		boolean expected = true;
-		boolean actual = BasicJavaP2.isUpper('F');
+		boolean actual = BasicJavaP2Dad.isUpper('F');
 		
 		assertEquals("Checking if F is upper case letter", expected, actual);
 	}
@@ -111,7 +111,7 @@ public class BasicJavaP2TestForEclipseDad {
 		assertFalse("Don't use the Character class in your code.",usesLibrary("Character"));
 		
 		boolean expected = false;
-		boolean actual = BasicJavaP2.isUpper('a');
+		boolean actual = BasicJavaP2Dad.isUpper('a');
 		
 		assertEquals("Checking if a is upper case letter", expected, actual);
 	}
@@ -121,7 +121,7 @@ public class BasicJavaP2TestForEclipseDad {
 		assertFalse("Don't use the Character class in your code.",usesLibrary("Character"));
 		
 		boolean expected = false;
-		boolean actual = BasicJavaP2.isUpper('7');
+		boolean actual = BasicJavaP2Dad.isUpper('7');
 		
 		assertEquals("Checking if 7 is upper case letter", expected, actual);
 	}
@@ -132,7 +132,7 @@ public class BasicJavaP2TestForEclipseDad {
 		assertFalse("Don't use the Math library or the word Math in your code.",usesLibrary("Math"));
 		
 		double expected = 28;
-		double actual = BasicJavaP2.computePolynomial(0.0);
+		double actual = BasicJavaP2Dad.computePolynomial(0.0);
 		
 		assertEquals("Value of polynomial at 0.0", expected, actual, 0.00001);
 	}
@@ -142,7 +142,7 @@ public class BasicJavaP2TestForEclipseDad {
 		assertFalse("Don't use the Math library or the word Math in your code.",usesLibrary("Math"));
 		
 		double expected = 9857.7225;
-		double actual = BasicJavaP2.computePolynomial(100.15);
+		double actual = BasicJavaP2Dad.computePolynomial(100.15);
 		
 		assertEquals("Value of polynomial at 100.15", expected, actual, 0.00001);
 	}
@@ -152,7 +152,7 @@ public class BasicJavaP2TestForEclipseDad {
 		assertFalse("Don't use the Math library or the word Math in your code.",usesLibrary("Math"));
 		
 		double expected = 31.990756;
-		double actual = BasicJavaP2.computePolynomial(-1.234);
+		double actual = BasicJavaP2Dad.computePolynomial(-1.234);
 		
 		assertEquals("Value of polynomial at -1.234", expected, actual, 0.00001);
 	}
@@ -162,7 +162,7 @@ public class BasicJavaP2TestForEclipseDad {
 		assertFalse("Don't use the Math library or the word Math in your code.",usesLibrary("Math"));
 		
 		double expected = 97555156.0;
-		double actual = BasicJavaP2.computePolynomial(-9876.0);
+		double actual = BasicJavaP2Dad.computePolynomial(-9876.0);
 		
 		assertEquals("Value of polynomial at -9876.0", expected, actual, 0.00001);
 	}
@@ -172,7 +172,7 @@ public class BasicJavaP2TestForEclipseDad {
 		assertFalse("Don't use the Math library or the word Math in your code.",usesLibrary("Math"));
 		
 		double expected = 97515652.0;
-		double actual = BasicJavaP2.computePolynomial(9876.0);
+		double actual = BasicJavaP2Dad.computePolynomial(9876.0);
 		
 		assertEquals("Value of polynomial at 9876.0", expected, actual, 0.00001);
 	}
@@ -182,7 +182,7 @@ public class BasicJavaP2TestForEclipseDad {
 		assertFalse("Don't use the Math library or the word Math in your code.",usesLibrary("Math"));
 		
 		long expected = 6;
-		long actual = BasicJavaP2.floorAfterMult(2,3.0);
+		long actual = BasicJavaP2Dad.floorAfterMult(2,3.0);
 		
 		assertEquals("Value of floor(2 * 3.0)", expected, actual);
 	}
@@ -192,7 +192,7 @@ public class BasicJavaP2TestForEclipseDad {
 		assertFalse("Don't use the Math library or the word Math in your code.",usesLibrary("Math"));
 		
 		long expected = -6;
-		long actual = BasicJavaP2.floorAfterMult(2,-3.0);
+		long actual = BasicJavaP2Dad.floorAfterMult(2,-3.0);
 		
 		assertEquals("Value of floor(2 * -3.0)", expected, actual);
 	}
@@ -202,7 +202,7 @@ public class BasicJavaP2TestForEclipseDad {
 		assertFalse("Don't use the Math library or the word Math in your code.",usesLibrary("Math"));
 		
 		long expected = 6;
-		long actual = BasicJavaP2.floorAfterMult(2,3.25);
+		long actual = BasicJavaP2Dad.floorAfterMult(2,3.25);
 		
 		assertEquals("Value of floor(2 * 3.25)", expected, actual);
 	}
@@ -212,7 +212,7 @@ public class BasicJavaP2TestForEclipseDad {
 		assertFalse("Don't use the Math library or the word Math in your code.",usesLibrary("Math"));
 		
 		long expected = 31;
-		long actual = BasicJavaP2.floorAfterMult(5,6.2);
+		long actual = BasicJavaP2Dad.floorAfterMult(5,6.2);
 		
 		assertEquals("Value of floor(5 * 6.2)", expected, actual);
 	}
@@ -222,7 +222,7 @@ public class BasicJavaP2TestForEclipseDad {
 		assertFalse("Don't use the Math library or the word Math in your code.",usesLibrary("Math"));
 		
 		long expected = 34;
-		long actual = BasicJavaP2.floorAfterMult(5,6.999);
+		long actual = BasicJavaP2Dad.floorAfterMult(5,6.999);
 		
 		assertEquals("Value of floor(5 * 6.999)", expected, actual);
 	}
@@ -230,7 +230,7 @@ public class BasicJavaP2TestForEclipseDad {
 	@Test
 	public void test_containsAllChars_bothEmptyString() {
 		boolean expected = true;
-		boolean actual = BasicJavaP2.containsAllChars("", "");
+		boolean actual = BasicJavaP2Dad.containsAllChars("", "");
 		
 		assertEquals("Testing countainsAllChars - both empty string", expected, actual);
 	}
@@ -238,7 +238,7 @@ public class BasicJavaP2TestForEclipseDad {
 	@Test
 	public void test_containsAllChars_firstIsEmptyString() {
 		boolean expected = false;
-		boolean actual = BasicJavaP2.containsAllChars("", "a");
+		boolean actual = BasicJavaP2Dad.containsAllChars("", "a");
 		
 		assertEquals("Testing countainsAllChars - empty string, non-empty string", expected, actual);
 	}
@@ -246,7 +246,7 @@ public class BasicJavaP2TestForEclipseDad {
 	@Test
 	public void test_containsAllChars_secondEmptyString() {
 		boolean expected = true;
-		boolean actual = BasicJavaP2.containsAllChars(" ", "");
+		boolean actual = BasicJavaP2Dad.containsAllChars(" ", "");
 		
 		assertEquals("Testing countainsAllChars - non-empty string, empty string", expected, actual);
 	}
@@ -254,7 +254,7 @@ public class BasicJavaP2TestForEclipseDad {
 	@Test
 	public void test_containsAllChars_containsAll() {
 		boolean expected = true;
-		boolean actual = BasicJavaP2.containsAllChars("This is a test", "ti sh");
+		boolean actual = BasicJavaP2Dad.containsAllChars("This is a test", "ti sh");
 		
 		assertEquals("Testing countainsAllChars - 'This is a test', 'ti sh'", expected, actual);
 	}
@@ -262,7 +262,7 @@ public class BasicJavaP2TestForEclipseDad {
 	@Test
 	public void test_containsAllChars_containsNone() {
 		boolean expected = false;
-		boolean actual = BasicJavaP2.containsAllChars("Another string for testing", "c/tmkl");
+		boolean actual = BasicJavaP2Dad.containsAllChars("Another string for testing", "c/tmkl");
 		
 		assertEquals("Testing countainsAllChars - 'Another string for testing', 'c/tmkl'", expected, actual);
 	}
@@ -270,7 +270,7 @@ public class BasicJavaP2TestForEclipseDad {
 	@Test
 	public void test_containsAllChars_containsSome() {
 		boolean expected = false;
-		boolean actual = BasicJavaP2.containsAllChars("This is a test", "ispa");
+		boolean actual = BasicJavaP2Dad.containsAllChars("This is a test", "ispa");
 		
 		assertEquals("Testing countainsAllChars - 'This is a test', 'ispa'", expected, actual);
 	}
