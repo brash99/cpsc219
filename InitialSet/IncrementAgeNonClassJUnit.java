356456562955150a20da2fb7ceb6f3a9ec1845aa
Edward Brash <brash@jlab.org>
2020-01-27 11:06:14 -0500

    My original test file

---
+++ InitialSet/IncrementAgeNonClassJUnit.java
@@ -0,0 +1,40 @@
+/*******************************
+ *
+ * Application: IncrementAgeNonClassJUnit 
+ * Purpose: Demonstrate the problems with pass by value vs. pass by reference
+ * 
+ * Note: When we learn about objects and classes, this will come up again, and there
+ *       is a really cool way to make our life easier!!!
+ *
+ * @author brash
+ * Date:  January 5, 2020
+ *
+ *******************************/
+
+public class IncrementAgeNonClassJUnit {
+
+    static void PlusAgeUseless(int age) {
+        age = age + 1;
+    }
+    
+    static int PlusAge(int age) {
+        age = age + 1;
+        return age;
+    }
+
+    public static int main(String [] args) {
+
+        int age = 25;
+        System.out.println("Current age = " + age);
+
+        PlusAgeUseless(age);
+        System.out.println("New age = " + age);
+        
+        age = PlusAge(age);
+        System.out.println("New age = " + age);
+
+	return age;
+    
+    }
+
+}
---
+++ InitialSet/IncrementAgeNonClassJUnitTest.java
@@ -0,0 +1,106 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class IncrementAgeNonClassJUnitTest {
+	String filename = "IncrementAgeNonClassJUnit.java";
+
+	private boolean containsImportStatement() {
+		boolean containsImport = false;
+		
+		try {
+			BufferedReader in = new BufferedReader(new FileReader(filename));
+			String line = in.readLine();
+			while (line != null && !containsImport) {
+				if (line.matches("import+\\s.*")) {
+					containsImport = true;
+				} 
+				line = in.readLine();
+			}
+			in.close();
+		} catch (FileNotFoundException e) {
+		} catch (IOException e) {
+		}
+		return containsImport;
+	}
+	
+	/**
+	Checks if the specified library is used anywhere in the code tested.  It checks
+	for the word exactly.  If there is a variable name that contains the library name,
+	this will result in a false positive.
+	*/
+	private boolean usesLibrary(String libraryName) {
+		boolean usesLibrary = false;
+		
+		try {
+			BufferedReader in = new BufferedReader(new FileReader(filename));
+			String line = in.readLine();
+			while (line != null && !usesLibrary) {
+				if (line.contains(libraryName)) {
+					usesLibrary = true;
+				} 
+				line = in.readLine();
+			}
+			in.close();
+		} catch (FileNotFoundException e) {
+		} catch (IOException e) {
+		}
+		return usesLibrary;	
+	}
+	
+	/**
+		Checks if the coding construct is used in the class we're testing.  It expects the 
+		construct to be preceded and followed by white space or parenthesis.
+	*/
+	private boolean usesConstruct(String constructName) {
+		boolean usesConstruct = false;
+		
+		try {
+			BufferedReader in = new BufferedReader(new FileReader(filename));
+			String line = in.readLine();
+			while (line != null && !usesConstruct) {
+				if (line.matches(".*\\s+if+[\\s+,(].*")) {
+					usesConstruct = true;
+				} 
+				line = in.readLine();
+			}
+			in.close();
+		} catch (FileNotFoundException e) {
+		} catch (IOException e) {
+		}
+		return usesConstruct;	
+	}
+		
+	@Test
+	public void test_plus_age() {
+		
+		long expected = 26;
+		long actual = IncrementAgeNonClassJUnit.PlusAge(25);
+		
+		assertEquals("Increment age 25 by 1: ", expected, actual);
+	}
+	
+	@Test
+	public void test_plus_age2() {
+		
+		long expected = 54;
+		long actual = IncrementAgeNonClassJUnit.PlusAge(53);
+		
+		assertEquals("Increment age 53 by 1:", expected, actual);
+	}
+	
+	@Test
+	public void test_main() {
+		int expected = 26;
+		String[] myargs = {""};
+		int actual = IncrementAgeNonClassJUnit.main(myargs);
+		
+		assertEquals("Main Program return value (age = 26)", expected, actual, 0.0001);
+	}
+	
+}
