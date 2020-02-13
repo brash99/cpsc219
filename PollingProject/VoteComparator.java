73e892ba450a0ccd43aead0162a14cbe7d90c7fc
Edward Brash <brash@jlab.org>
2020-02-12 19:49:32 -0500

    Add comparator classes for multi-field sorting

---
+++ PollingProject/VoteComparator.java
@@ -0,0 +1,10 @@
+import java.util.Comparator;
+ 
+public class VoteComparator implements Comparator<Party> {
+ 
+    @Override
+    public int compare(Party p1, Party p2) {
+	       return (p1.getProjectedPercentageOfVotes() > p2.getProjectedPercentageOfVotes() ? -1 : 
+	    	   (p1.getProjectedPercentageOfVotes() == p2.getProjectedPercentageOfVotes() ? 0 : 1));
+    }
+}
