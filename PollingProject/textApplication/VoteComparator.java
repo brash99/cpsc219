372dd3a5032611f983102c4d8b13d989dcaac389
Edward Brash <brash@jlab.org>
2020-02-28 11:56:36 -0500

    Merging updates

---
+++ PollingProject/textApplication/VoteComparator.java
@@ -0,0 +1,12 @@
+package textApplication;
+
+import java.util.Comparator;
+ 
+public class VoteComparator implements Comparator<Party> {
+ 
+    @Override
+    public int compare(Party p1, Party p2) {
+	       return (p1.getProjectedPercentageOfVotes() > p2.getProjectedPercentageOfVotes() ? -1 : 
+	    	   (p1.getProjectedPercentageOfVotes() == p2.getProjectedPercentageOfVotes() ? 0 : 1));
+    }
+}
