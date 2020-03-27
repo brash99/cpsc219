d0a9c92ec1163991e19d15d2d8297517eccd8824
Edward Brash <brash@jlab.org>
2020-03-27 08:51:34 -0400

    Add catches for exceptions

--- InitialSet/FileExercises.java
+++ InitialSet/FileExercises.java
@@ -1,4 +1,3 @@
-import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.DataInputStream;
@@ -10,31 +9,42 @@ public class FileExercises {
 	public static boolean is3ByteRGB(String fileName) throws FileNotFoundException, IOException {
 		
 		boolean result = true;
-		File file = new File(fileName);
-		FileInputStream fileInputStream = new FileInputStream(file);
-		DataInputStream in = new DataInputStream(fileInputStream);
+	
+		try {
+			
+			File file = new File(fileName);
+			FileInputStream fileInputStream = new FileInputStream(file);
+			DataInputStream in = new DataInputStream(fileInputStream);
 		
-		int rows, columns;
-		rows = in.readInt();
-		columns = in.readInt();
-		System.out.println(rows + " " + columns);
+			int rows, columns;
+			rows = in.readInt();
+			columns = in.readInt();
+			//System.out.println(rows + " " + columns);
 		
-		for (int r=0; r<rows; r++) {
-			for (int c=0; c<columns; c++) {
-				int redValue = in.readInt();
-				if (redValue < 0 || redValue > 255) {
-					result = false;
+			for (int r=0; r<rows; r++) {
+				for (int c=0; c<columns; c++) {
+					int redValue = in.readInt();
+					if (redValue < 0 || redValue > 255) {
+						result = false;
+					}
+					int greenValue = in.readInt();
+					if (greenValue < 0 || greenValue > 255) {
+						result = false;
+					}
+					int blueValue = in.readInt();
+					if (blueValue < 0 || blueValue > 255) {
+						result = false;
+					}
+					//System.out.println("row = " + r + "  column = " + c + " " + redValue + "," + greenValue + "," + blueValue);
 				}
-				int greenValue = in.readInt();
-				if (greenValue < 0 || greenValue > 255) {
-					result = false;
-				}
-				int blueValue = in.readInt();
-				if (blueValue < 0 || blueValue > 255) {
-					result = false;
-				}
-				System.out.println("row = " + r + "  column = " + c + " " + redValue + "," + greenValue + "," + blueValue);
 			}
+			
+			in.close();
+			
+		} catch (FileNotFoundException f) {
+			System.out.println("File " + fileName + " not found ... check file name!");
+		} catch (IOException e) {
+			System.out.println("IO Exception reading " + fileName);
 		}
 		
 		return result;
@@ -42,7 +52,11 @@ public class FileExercises {
 	}
 
 	public static void main(String[] args) throws FileNotFoundException,IOException {
-		String fileName = "testFile2.bin";
-		System.out.println("File result: " + is3ByteRGB(fileName));
+		String fileName1 = "testFile1.bin";
+		System.out.println("File 1 result: " + is3ByteRGB(fileName1));
+		String fileName2 = "testFile2.bin";
+		System.out.println("File 2 result: " + is3ByteRGB(fileName2));
+		String fileName3 = "testFile3.bin";
+		System.out.println("File 3 result: " + is3ByteRGB(fileName3));
 	}
 }
