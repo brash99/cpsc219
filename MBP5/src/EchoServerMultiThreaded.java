d4bd9d5b8995fd03ea2651fb9f51b04d5c111f77
Edward Brash <brash@jlab.org>
2020-04-25 12:22:45 -0400

    updates

--- MBP5/src/EchoServerMultiThreaded.java
+++ MBP5/src/EchoServerMultiThreaded.java
@@ -14,9 +14,12 @@ public class EchoServerMultiThreaded {
 			System.exit(1);
 		}
 
+		// listen forever
 		while (true) {
 			Socket clientSocket = null;
 			try {
+				// Ask the main computer, over the serverSocket, if there
+				// are any clients that would like to communicate
 				clientSocket = serverSocket.accept();
 			} catch (IOException e) {
 				System.out.println("Accept failed: " + PORT + ", " + e);
@@ -36,15 +39,21 @@ class EchoServerThread extends Thread {
 		this.socket = socket;
 	}
 	
+	// start communicating with the client through the clientSocket
 	public void run() {
 		try {
+			
+			// establish a way to read and write
 			BufferedReader is = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			PrintWriter os = new PrintWriter(new BufferedOutputStream(socket.getOutputStream()));
+			
 			String inputLine, outputLine;
+			
 			while ((inputLine = is.readLine()) != null) {
 				outputLine = inputLine;
 				os.println(outputLine); //just send back whatever was sent to us 
 				os.flush();
+				
 				if (outputLine.equals("quit")) break;
 			}
 			os.close();
